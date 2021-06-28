42cb891e246182b0f75e4925eae14fd26fe71878
Matheus Laurindo <matheus104521@gmail.com>
2021-06-28 15:29:17 -0300

    Fazendo exercicios

--- exercises/bloco_4/dia_3/script.js
+++ exercises/bloco_4/dia_3/script.js
@@ -2,10 +2,38 @@ let n = 5;
 let symbol = '*';
 let line = '';
 
+/*
 for (let index = 0; index < n; index += 1) {
     line = line + symbol;
 }
 
 for( let index2 = 0; index2 < n; index2 += 1) {
     console.log(line);
+}  */
+
+/***************
+ * Exercicio 2
+ ***************
+
+for (let index = 0; index < n; index += 1) {
+    line = line + symbol;
+
+    console.log(line);
+}
+ 
+*/
+
+/***************
+ * Exercicio 3
+ ***************/
+
+let rowBlank =  '';
+
+for (let index = 0; index < n; index += 1) {
+    line += symbol;
+
+    for(let i = 4; i >= 1; i -= 1) {  
+        rowBlank += '';
+    }
+    console.log(rowBlank, line);
 }
