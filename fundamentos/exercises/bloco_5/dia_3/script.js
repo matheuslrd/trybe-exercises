45eba7d9b49159705a3111075c6eab391245848a
Matheus Laurindo <matheus104521@gmail.com>
2021-07-06 22:36:00 -0300

    Conclui exercicio 1

--- fundamentos/exercises/bloco_5/dia_3/script.js
+++ fundamentos/exercises/bloco_5/dia_3/script.js
@@ -1,5 +1,5 @@
 function createDaysOfTheWeek() {
-    const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
+    const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado'];
     const weekDaysList = document.querySelector('.week-days');
   
     for (let index = 0; index < weekDays.length; index += 1) {
@@ -14,3 +14,36 @@ function createDaysOfTheWeek() {
   createDaysOfTheWeek();
   
   // Escreva seu código abaixo.
+
+function createDezDays() {
+    const dezDays = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+    const dezDaysList = document.querySelector('#days');
+
+    //Percorre o array de dias
+    for(let key of dezDays) {
+        //Atribui cada elemento percorrido a days
+        let days = key;
+
+        //Cria o elemento li, adiciona a classe day 
+        //Adiciona o texto days que são os dias que devem ser adicionados
+        let dayListItem = document.createElement('li');
+        dayListItem.className = 'day';
+
+        //Faz a validação dos dias que são feriados e adiciona uma classe somente a eles
+        if (days === 24 || days === 25 || days === 31) {
+            dayListItem.classList.add('holiday');
+        };
+
+        if (days === 4 || days === 11 || days === 18 || days === 25) {
+            dayListItem.classList.add('friday');
+        }
+
+        //Adiciona há variavel dayListItem o texto days, contendo todos os dias do array
+        dayListItem.innerHTML = days;
+
+        //Adiciona o item formatado há ul do HTML
+        dezDaysList.appendChild(dayListItem);
+    }
+}
+
+createDezDays();
