4487a212d72867450c588245a9b5ce68c251feb5
Matheus Laurindo <matheus104521@gmail.com>
2021-07-26 14:19:01 -0300

    Conclui a primeira parte dos exercicios

--- fundamentos/exercises/bloco-7/dia-2/script.js
+++ fundamentos/exercises/bloco-7/dia-2/script.js
@@ -44,14 +44,27 @@ const customerInfo = (order) => {
   const apartment = order[address].apartment;
   const deliveryPerson = order.order.delivery.deliveryPerson;
 
-  console.log(`Olá ${deliveryPerson}, entrega para ${orderName}, Telefone: ${phoneNumber}, R. ${street}, N ${num}, AP: ${apartment}`);
+  /* return console.log(`Olá ${deliveryPerson}, entrega para ${orderName}, Telefone: ${phoneNumber}, R. ${street}, N ${num}, AP: ${apartment}`); */
 }
 
 customerInfo(order);
 
 const orderModifier = (order) => {
   // Adicione abaixo as informações necessárias.
+  order.name = 'Luiz Silva';
+  const name = order.name;
 
+  const listOrderPizzas = Object.keys(order.order.pizza);
+  const marguerita = listOrderPizzas[0];
+  const peperoni = listOrderPizzas[1];
+
+  const listOrderDrinks = Object.values(order.order.drinks.coke);
+  const cocaCola = listOrderDrinks[0];
+  
+  order.payment = 'R$ 50.00';
+  const total = order.payment;
+  
+  return console.log( `Olá ${name}, o total do seu pedido de ${marguerita}, ${peperoni} e ${cocaCola} é de ${total}`);
 }
 
 orderModifier(order);
