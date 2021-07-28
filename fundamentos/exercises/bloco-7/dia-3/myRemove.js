20db47c5b351c18cdeae020b5416d93f38fbb506
Matheus Laurindo <matheus104521@gmail.com>
2021-07-28 15:42:37 -0300

    Conclui

--- fundamentos/exercises/bloco-7/dia-3/myRemove.js
+++ fundamentos/exercises/bloco-7/dia-3/myRemove.js
@@ -10,4 +10,10 @@ function myRemove(arr, item) {
   return newArr;
 }
 
+const expected = myRemove([1, 2, 3, 4], 3);
+console.log(expected);
+
 // implemente seus testes aqui
+assert.deepStrictEqual(myRemove([1, 2, 3, 4], 3), expected);
+assert.notDeepStrictEqual(myRemove([1, 2, 3, 4], 3), [1, 2, 3, 4], 'Item não removido!');
+assert.deepStrictEqual(myRemove([1, 2, 3, 4], 3), [1, 2, 3, 4], 'Item não sofreu alteração');
