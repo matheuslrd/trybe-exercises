825f96ab65dd79ba84f4b1c60c6599c3c49c317d
Matheus Laurindo <matheus104521@gmail.com>
2021-07-29 20:46:56 -0300

    Conclui o exercicio 4

--- fundamentos/bloco-8/dia-2/exercises/exercise1.js
+++ fundamentos/bloco-8/dia-2/exercises/exercise1.js
@@ -85,7 +85,7 @@ function smallerName() {
 }
 assert.strictEqual(smallerName(), 'Duna');
 
-const expectedResult = {
+const expectedResultt = {
   author: {
     birthYear: 1948,
     name: 'George R. R. Martin',
@@ -108,4 +108,63 @@ function getNamedBook() {
   return book26caractere;
 }
 
-assert.deepStrictEqual(getNamedBook(), expectedResult);
+assert.deepStrictEqual(getNamedBook(), expectedResultt);
+
+const expectedResult = [
+  {
+    id: 1,
+    name: 'As Crônicas de Gelo e Fogo',
+    genre: 'Fantasia',
+    author: { name: 'George R. R. Martin', birthYear: 1948 },
+    releaseYear: 1991,
+  },
+  {
+    id: 5,
+    name: 'A Coisa',
+    genre: 'Terror',
+    author: { name: 'Stephen King', birthYear: 1947 },
+    releaseYear: 1986,
+  },
+  {
+    id: 4,
+    name: 'Duna',
+    genre: 'Ficção Científica',
+    author: { name: 'Frank Herbert', birthYear: 1920 },
+    releaseYear: 1965,
+  },
+  {
+    id: 2,
+    name: 'O Senhor dos Anéis',
+    genre: 'Fantasia',
+    author: { name: 'J. R. R. Tolkien', birthYear: 1892 },
+    releaseYear: 1954,
+  },
+  {
+    id: 3,
+    name: 'Fundação',
+    genre: 'Ficção Científica',
+    author: { name: 'Isaac Asimov', birthYear: 1920 },
+    releaseYear: 1951,
+  },
+  {
+    id: 6,
+    name: 'O Chamado de Cthulhu',
+    genre: 'Terror',
+    author: { name: 'H. P. Lovecraft', birthYear: 1890 },
+    releaseYear: 1928,
+  },
+];
+
+function booksOrderedByReleaseYearDesc() {
+  // escreva aqui seu código
+  return books.sort( (a, b) => {
+    if (a.releaseYear > b.releaseYear) {
+      return -1;
+    } else if (a.releaseYear < b.releaseYear) {
+      return 1;
+    } 
+    return 0;
+  });
+}
+
+assert.deepStrictEqual(booksOrderedByReleaseYearDesc(), expectedResult);
