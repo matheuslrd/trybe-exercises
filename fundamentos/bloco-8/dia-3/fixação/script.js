0d93777ab2a21925fa7d127dddad5476ffb80072
Matheus Laurindo <matheus104521@gmail.com>
2021-07-30 18:27:07 -0300

    Conclui exercicio de fixaçao 1

---
+++ fundamentos/bloco-8/dia-3/fixação/script.js
@@ -0,0 +1,6 @@
+const products = ['Arroz', 'Feijão', 'Alface', 'Tomate'];
+const prices = [2.99, 3.99, 1.5, 2];
+
+const listProducts = products.map((product, index) => ({[product]: prices[index]}));
+
+console.log(listProducts);
