cbceef844d9070b1bc4eb1d956d66f78ac75d232
Matheus Laurindo <matheus104521@gmail.com>
2021-07-30 20:35:24 -0300

    Conclui o exercicio 4

--- fundamentos/bloco-8/dia-3/exercises/exercise4.js
+++ fundamentos/bloco-8/dia-3/exercises/exercise4.js
@@ -64,40 +64,35 @@ const books = [
 ];
 
 const expectedResult = [
-  { 
-    id: 1,
-    name: 'As Crônicas de Gelo e Fogo',
-    genre: 'Fantasia',
-    author: { name: 'George R. R. Martin', birthYear: 1948 },
-    releaseYear: 1991
-  },
   {
-    id: 2,
-    name: 'O Senhor dos Anéis',
-    genre: 'Fantasia',
-    author: { name: 'J. R. R. Tolkien', birthYear: 1892 },
-    releaseYear: 1954
+    id: 6,
+    name: 'O Chamado de Cthulhu',
+    genre: 'Terror',
+    author: { name: 'H. P. Lovecraft', birthYear: 1890 },
+    releaseYear: 1928,
   },
   {
     id: 3,
     name: 'Fundação',
     genre: 'Ficção Científica',
     author: { name: 'Isaac Asimov', birthYear: 1920 },
-    releaseYear: 1951
+    releaseYear: 1951,
   },
   {
-    id: 4,
-    name: 'Duna',
-    genre: 'Ficção Científica',
-    author: { name: 'Frank Herbert', birthYear: 1920 },
-    releaseYear: 1965
-  }
+    id: 2,
+    name: 'O Senhor dos Anéis',
+    genre: 'Fantasia',
+    author: { name: 'J. R. R. Tolkien', birthYear: 1892 },
+    releaseYear: 1954,
+  },
 ];
 
-function fantasyOrScienceFiction() {
+const yearDate = new Date().getFullYear();
+
+function oldBooksOrdered() {
   // escreva seu código aqui
-  const objFantasyScience = books.filter((book) => book.genre === 'Fantasia' || book.genre === 'Ficção Científica');
-  return objFantasyScience;
+  const booksAge60 = books.filter((book) => yearDate - book.releaseYear >= 60 );
+  return booksAge60.sort((a, b) => a.releaseYear - b.releaseYear);
 }
 
-assert.deepStrictEqual(fantasyOrScienceFiction(), expectedResult);
+assert.deepStrictEqual(oldBooksOrdered(), expectedResult);
