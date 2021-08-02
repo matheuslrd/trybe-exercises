b8667b9727719017567f9d55c9ab7cdf23387a97
Matheus Laurindo <matheus104521@gmail.com>
2021-08-02 16:01:39 -0300

    Refatora exercicio de fixaçao

--- fundamentos/bloco-8/dia-4/fixacao/script.js
+++ fundamentos/bloco-8/dia-4/fixacao/script.js
@@ -1,8 +1,10 @@
 const numbers = [18, 19, 23, 53, 4, 5, 76, 23, 54];
 
-const getPairs = numbers.filter((num) => num % 2 === 0);
+//Pega os números pares
+const arrGetPairs = numbers.filter((num) => num % 2 === 0);
+
+//FUnção para somar todos numeros de um array
 const sumPairs = (result, number) => result + number;
+const showSumPairs = (array) => array.reduce(sumPairs);
 
-const showSumPairs = numbers.reduce(sumPairs);
-
-console.log(showSumPairs);
+console.log(showSumPairs(arrGetPairs));
