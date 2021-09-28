9aac7f8f17d392a54431b9cbf4ce100603b83cd5
Matheus Laurindo <matheus104521@gmail.com>
2021-09-28 18:49:43 -0300

    Conclui fixacao

--- front-end/bloco-15/fixacao/dia-1/fixacao.rtl-react-router/src/App.test.js
+++ front-end/bloco-15/fixacao/dia-1/fixacao.rtl-react-router/src/App.test.js
@@ -1,8 +1,24 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import renderWithRouter from './renderWithRouter';
+import { fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('Testa a aplicação inteira', () => {
+  it('deve renderizar o componente App', () => {
+    const { getByText } = renderWithRouter(<App />);
+    const home = getByText(/Você está na página Início/);
+    expect(home).toBeInTheDocument();
+  });
+
+  it('deve renderizar a página sobre', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    const aboutLink = getByText('Sobre');
+    fireEvent.click(aboutLink);
+
+    const pathname = history.location.pathname;
+    expect(pathname).toBe('/about');
+
+    const aboutText = getByText('Você está na página Sobre')
+    expect(aboutText).toBeInTheDocument();
+  });
+})
