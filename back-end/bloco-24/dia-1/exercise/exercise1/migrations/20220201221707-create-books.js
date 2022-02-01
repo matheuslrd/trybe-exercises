fbdb244b88018e730d5de610a8d106699a385ea5
Matheus Laurindo <matheus104521@gmail.com>
2022-02-01 20:38:04 -0300

    Cria a tabela Books e adiciona atributos

--- back-end/bloco-24/dia-1/exercise/exercise1/migrations/20220201221707-create-books.js
+++ back-end/bloco-24/dia-1/exercise/exercise1/migrations/20220201221707-create-books.js
@@ -1,21 +1,35 @@
 'use strict';
 
-module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+const Books = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Books', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        type: Sequelize.INTEGER,
+        primaryKey: true
+      },
+      title: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      author: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      pageQuantity: {
+        allowNull: true,
+        type: Sequelize.INTEGER,
+      },
+      createAt: {
+        allowNull: false,
+        type: Sequelize.date
+      },
+    });
   },
-
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
-  }
+  down: async (queryInterface, _Sequelize) => {
+    await queryInterface.dropTable('Books');
+  },
 };
+
+module.exports = Books;
