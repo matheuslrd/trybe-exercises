1166584d5321fc8c23b36a9dd6c32404fedcd814
Matheus Laurindo <matheus104521@gmail.com>
2022-02-17 15:54:29 -0300

    Conclui funcao de calcular losango e termina exercicios de fixacao

--- back-end/bloco-26/dia-1/fixacao/exercises.ts
+++ back-end/bloco-26/dia-1/fixacao/exercises.ts
@@ -5,3 +5,8 @@ const greeter = (name:string) => {
 const personAge = (name:string, age:number) => {
   return `${name} tem ${age} anos`;
 };
+
+const calcularLosango = (largerLozenge: number, lesserLozenge:number) => {
+  return (largerLozenge * lesserLozenge) / 2;
+};
+
