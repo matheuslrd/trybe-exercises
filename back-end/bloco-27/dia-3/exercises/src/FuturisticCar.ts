4cf837b16c3cc30ca03ed47e9378e58310ca5792
Matheus Laurindo <matheus104521@gmail.com>
2022-03-10 08:32:54 -0300

    Cria arquivos de futuristic car para realizaçao dos exercicios

---
+++ back-end/bloco-27/dia-3/exercises/src/FuturisticCar.ts
@@ -0,0 +1,8 @@
+import { IVehicle } from "./interfaces/Vehicle/IVehicle";
+
+// ./FuturisticCar.ts
+export default class FuturisticCar implements IVehicle {
+  drive(): void { console.log('Drive a futuristic car'); }
+
+  fly(): void { console.log('Flying a futuristic car'); }
+}
